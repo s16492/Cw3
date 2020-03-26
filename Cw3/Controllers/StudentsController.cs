4c489fc32d2c7beba0d67e34c5c9e619ec89fb35
s16492 <s16492@pjwstk.edu.pl>
2020-03-26 23:53:16 +0100

    ćw4

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -1,11 +1,11 @@
-﻿
-using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Cw3.DAL;
+using System.Data.SqlClient;
 using Cw3.Models;
 using Microsoft.AspNetCore.Mvc;
+using Cw3.DAL;
+
 
 namespace Cw3.Controllers
 {
@@ -14,57 +14,87 @@ namespace Cw3.Controllers
     public class StudentsController : ControllerBase
     {
 
-        private readonly IDBService _dbService;
+        //private readonly IDBService _dbService;
 
-        public StudentsController(IDBService dbService)
-        {
-            _dbService = dbService;
-        }
+        //public StudentsController(IDBService dbService)
+        //{
+        //    _dbService = dbService;
+        //}
+
+        //[HttpGet]
+        //public IActionResult GetStudent(String orderBy)
+        //{
+        //    return Ok(_dbService.GetStudents());
+        //}
+
+
+
+        //[HttpGet("{id}")]
+        //public IActionResult GetStudent(int id)
+        //{
+        //    if (id == 1)
+        //    {
+        //        return Ok("Kowalski");
+        //    }
+        //    else if (id == 2)
+        //    {
+        //        return Ok("Malewski");
+        //    }
+
+        //    return NotFound("Nie znaleziono studenta");
+        //}
+
+        //[HttpPost]
+        //public IActionResult CreateSudent(Student student)
+        //{
+        //    student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+        //    return Ok(student);
+        //}
+
+        //        [HttpPut("{id:int}")]
+        //public IActionResult UpdateStudent(int id)
+        //{
+
+        //    return Ok($"Aktualizacja dokonczona");
+        //}
+
+        //[HttpDelete("{id:int}")]
+        //public IActionResult RemoveStudent(int id)
+        //{
+
+        //    return Ok($"Usuwanie ukonczone");
+        //}
+
 
         [HttpGet]
-        public IActionResult GetStudent(String orderBy)
+        public IActionResult GetStudents()
         {
-            return Ok(_dbService.GetStudents());
-        }
-
-
-
-        [HttpGet("{id}")]
-        public IActionResult GetStudent(int id)
-        {
-            if (id == 1)
+            var stud = new List<Student>();
+            using (var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s16492;Integrated Security=True"))
+            using (var com = new SqlCommand())
             {
-                return Ok("Kowalski");
+                string id = "1";
+
+                com.Connection = con;
+                com.CommandText = "select * from Student where IndexNumber=@id";
+                com.Parameters.AddWithValue("id", id);
+                con.Open();
+                var dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var st = new Student();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = DateTime.Parse(dr["BirthDate"].ToString());
+                    st.IdEnrollment = int.Parse(dr["IdEnrollment"].ToString());
+                    stud.Add(st);
+                }
             }
-            else if (id == 2)
-            {
-                return Ok("Malewski");
-            }
 
-            return NotFound("Nie znaleziono studenta");
+            return Ok(stud);
+
         }
-
-        [HttpPost]
-        public IActionResult CreateSudent(Student student)
-        {
-            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
-            return Ok(student);
-        }
-
-                [HttpPut("{id:int}")]
-        public IActionResult UpdateStudent(int id)
-        {
-
-            return Ok($"Aktualizacja dokonczona");
-        }
-
-        [HttpDelete("{id:int}")]
-        public IActionResult RemoveStudent(int id)
-        {
-
-            return Ok($"Usuwanie ukonczone");
-        }
-
 
 
 
